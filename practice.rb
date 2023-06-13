c62bfd3f338e799cd1a5fad448a6a8dcc483cf35
Ryota Kimura <kimuchan173049@gmail.com>
2023-06-13 00:38:30 +0000

    kadai 1st No.10

--- practice.rb
+++ practice.rb
@@ -1,38 +1,62 @@
-#1st No.11 課題
+# 1st No.9 課題
 
-name = "Ryota"
-#name = "Taro"
+a=3
+b=7
+puts a+b
 
-if name == "Ryota"
-  puts "私は、" + name + "です。"
-else
-  puts name + "ではありません。"
-end
+array_month=["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"]
+puts array_month[7]
+
+array_month={
+  "a" => "1月",
+  "b" => "2月",
+  "c" => "3月",
+  "d" => "4月",
+  "e" => "5月",
+  "f" => "6月",
+  "g" => "7月",
+  "h" => "8月",
+  "i" => "9月",
+  "j" => "10月",
+  "k" => "11月",
+  "l" => "12月",
+}
+
+puts array_month["h"]
+
+hello="Hello,"
+name="Ryota"
+world="'s World!"
+
+puts hello+name+world
 
 
-total=0
-puts total
+omusubi_tech="tech"
+omusubi_tech.insert(0, "omusubi ")
+puts omusubi_tech
 
-for i in 0..10000
-  total += i
-end
-puts total
-
-
-fruits = ["apple","banana","orange","lemon","strawberry"]
-puts fruits.length
-fruits.each do |fruit|
-  puts fruit
-end
 
 #応用課題
-start = 1
-# for文の終わりの値を定義する
-end_num = 100
+calendar_2023 = {
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+}
 
-(start..end_num).each do |i|
-  # 5で割り切れたら{}内を実行する
-  if i % 5 == 0
-    puts i
-  end
-end
+puts calendar_2023["December"]
+
+#変数名　誤 2023_calendar　正 calendar_2023
+#２月　　誤 、　           正 ,
+#６月　　誤 = 　           正 =>
+#１０月  誤 10月  　　　　 正 "10月"
+#Puts    誤 December  　　 正 "December"
+
